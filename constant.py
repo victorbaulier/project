7e6dbca5f3a5c336305c9995de47d1bc1ee65395
victorbaulier <victorbaulier@live.fr>
2016-05-20 15:17:39 +0200

    Add files via upload

---
+++ constant.py
@@ -0,0 +1,10 @@
+﻿# Créé par victor, le 20/05/2016 en Python 3.2
+sprite_number = 15
+sprite_size = 30
+window_width= (sprite_number * sprite_size)
+
+
+#Listes des images du jeu
+
+picture_background = "pictures/background.png"
+picture_wall = "pictures/wall.jpg"
---
+++ main.py
@@ -0,0 +1,16 @@
+﻿# Créé par victor, le 20/05/2016 en Python 3.2
+import pygame
+from pygame.locals import *
+
+from classs import *
+from constant import *
+
+
+pygame.init()
+
+window=pygame.display.set_mode((window_width, window_width))
+
+background=pygame.image.load(picture_background).convert()
+level= Level('l1')
+level.create()
+level.print(window)
