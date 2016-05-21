70a446e4a686c69bfa36fb23259a3d88ce34d58e
victorbaulier <victorbaulier@live.fr>
2016-05-21 19:05:24 +0200

    the movement right is added to the class

--- classs.py
+++ classs.py
@@ -1,4 +1,4 @@
-﻿# Créé par victor, le 20/05/2016 en Python 3.2
+# Créé par victor, le 20/05/2016 en Python 3.2
 
 import pygame
 from pygame.locals import *
@@ -11,12 +11,13 @@ class Level:
         self.structure=0
 
     def create(self):
+        #we create a structure which will contain all the datas from the file
         with open(self.file, "r") as file:
             level_structure = []
             for(line)in file:
                 level_line=[]
+        # each element from the structure is line
                 for sprite in line:
-                    #on ignore les "\n" de fin de ligne
                     if sprite!='\n':
                         #letters are add to the line's array
                         level_line.append(sprite)
@@ -24,12 +25,13 @@ class Level:
                 level_structure.append(level_line)
             # we save this structure
             self.structure=level_structure
-            print(self.structure)
 
     def print(self, window):
 
         #images are loaded
         wall=pygame.image.load(picture_wall).convert()
+        #the programm reads the structure and find the different letters.
+
 
         line_number=0
         for line in self.structure:
@@ -37,15 +39,30 @@ class Level:
             for letter in line:
                 x=case_number*sprite_size
                 y=line_number*sprite_size
-                if letter=='m':
+        # x and y are used to find the exact place of the different letters in the structure
+        # and the images corresponding to the letters are placed in the window at the right coordonates
+                if letter=='w':
                     window.blit(wall,(x,y))
                 case_number+=1
             line_number+=1
-    
-    class Player:    #we creatte a class with all informations and fonctions of the worm
-
-    def __init__(self):
-
-        self.player=pygame.image.load('pictures/worms.png').convert_alpha() #picture of the worm
 
 
+class Player:
+
+    def __init__(self,level):
+
+        self.place_x=0
+        self.place_y=0
+        self.x=0
+        self.y=0
+
+        self.level=level
+        self.player=pygame.image.load('pictures/worms.png').convert_alpha()
+
+
+    def movements(self,direction):
+        if direction=='right':
+            if self.place_x<(sprite_number-1):      #checking if the next position isn't outside the window
+                if self.level.structure[self.place_y][self.place_x+1]!='w':     #checking if there is no wall on the next position
+                    self.place_x+=1
+                    self.x=self.place_x*sprite_size
