cc514cc3c22e9be6cee3487f5d4d8051733eb458
correthomas <corre.thomas@hotmail.fr>
2016-05-25 22:44:55 +0200

    this is tha last upload. we added the high scores and the main home page

--- constant.py
+++ constant.py
@@ -1,7 +1,15 @@
 # Créé par victor, le 20/05/2016 en Python 3.2
 sprite_number = 15
-sprite_size = 30
+sprite_size = 45
 window_width= (sprite_number * sprite_size)
+highscore_time1=6
+highscore_mvt1=23
+highscore_time2=6
+highscore_mvt2=22
+highscore_time3=6
+highscore_mvt3=19
+highscore_time4=6
+highscore_mvt4=13
 
 
 #Listes des images du jeu
@@ -9,3 +17,5 @@ window_width= (sprite_number * sprite_size)
 picture_background = "pictures/background.png"
 picture_wall = "pictures/wall.jpg"
 picture_teleport="pictures/teleport.jpg"
+picture_finish="pictures/finish.jpg"
+
